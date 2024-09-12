4a793a150ec71c03625b66a947f6c10028f88b55
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2024-09-12 09:42:27 -0400

    added example for final constants

---
+++ Unit One - Intro to Java/src/day1/ExampleFour.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class ExampleFour {
+public static void main(String[] args) {
+    final int x = 7;
+    int y = 8;
+    y = 9;
+    // x = 5; x is a constant (final) - you cannot change it
+
+}
+}
