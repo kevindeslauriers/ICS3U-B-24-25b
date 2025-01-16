323c90114628795a1b3e3973b5ec9604ec95f56f
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2025-01-16 10:37:14 -0500

    LEDStrip

---
+++ Unit Six - Arrays/src/day3/LEDStrip.java
@@ -0,0 +1,42 @@
+package day3;
+
+public class LEDStrip {
+    // Constant array representing available patterns
+    public static final String[] AVAILABLE_PATTERNS = {"random", "alternate", "red", "yellow", "green", "blue"};
+
+    // Attributes of the LEDStrip class
+    private LED[] leds;
+    private String pattern;
+
+    // Constructor stub to accept pattern and the number of LEDs in the strip
+    public LEDStrip(String pattern, int numLEDs) {
+        // Constructor code will be implemented here
+    }
+
+    // Method stub to count and return the number of LEDs that are not on
+    public int countOffLEDs() {
+        // Method code will be implemented here
+        return 0;
+    }
+
+    // Method stub to count and return the number of LEDs that are of a particular colour
+    public int countColourLEDs(String colour) {
+        // Method code will be implemented here
+        return 0;
+    }
+
+    // Getter and setter stub to change the pattern
+    public String getPattern() {
+        // Method code will be implemented here
+        return null;
+    }
+
+    public void setPattern(String pattern) {
+        // Method code will be implemented here
+    }
+
+    public void display(){
+        
+    }
+}
+
