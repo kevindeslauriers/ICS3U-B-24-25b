087bb8a61dbd9619325cb3d6e80cce7a2fdbaea2
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2025-01-17 09:32:15 -0500

    2023 q4 2D

---
+++ Unit Six - Arrays/src/day4/BoxOfCandy.java
@@ -0,0 +1,14 @@
+package day4;
+
+public class BoxOfCandy {
+    private Candy[][] box;
+
+    public boolean moveCandyToFirstRow(int col){
+
+    }
+
+    public Candy removeNextByFlavor(String flavor){
+
+    }
+
+}
---
+++ Unit Six - Arrays/src/day4/Candy.java
@@ -0,0 +1,14 @@
+package day4;
+
+public class Candy {
+
+    private String flavor;
+
+    public Candy(String flavor){
+        this.flavor = flavor;
+    }
+
+    public String getFlavor(){
+        return "";
+    }
+}
