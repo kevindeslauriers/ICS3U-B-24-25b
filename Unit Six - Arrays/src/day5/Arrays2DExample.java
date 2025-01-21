76e39df22c794b0900f0ccd3dd231b8dccd7afeb
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2025-01-21 09:11:36 -0500

    sumRows

--- Unit Six - Arrays/src/day5/Arrays2DExample.java
+++ Unit Six - Arrays/src/day5/Arrays2DExample.java
@@ -9,7 +9,28 @@ public class Arrays2DExample {
         System.out.println();
         populateArray(nums);
         print2DArray(nums);
+        int[] sumRows = rowSum(nums);
 
+        print1DArray(sumRows);
+
+    }
+
+    private static void print1DArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+
+    private static int[] rowSum(int[][] nums) {
+        int[] arr = new int[nums.length];
+
+        for (int row = 0; row < nums.length; row++) {
+            for (int col = 0; col < nums[row].length; col++) {
+                arr[row] += nums[row][col];
+            }
+        }
+
+        return arr;
     }
 
     private static void print2DArray(int[][] nums) {
