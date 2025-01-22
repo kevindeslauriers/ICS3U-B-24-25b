a4b99a0d9b405dd0f4b208832e99c8a1effdff07
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2025-01-22 11:37:19 -0500

    arraySum2D question v2

--- Unit Six - Arrays/src/day5/Arrays2DExample_v2.java
+++ Unit Six - Arrays/src/day5/Arrays2DExample_v2.java
@@ -4,18 +4,59 @@ public class Arrays2DExample_v2 {
     public static void main(String[] args) {
         int[][] nums = new int[5][3];
         populate2DArray(nums);
-
         print2DArray(nums);
         System.out.println();
-        System.out.println();
 
-        print1DArray(nums[0]);
+        int[] sumRows = arrRowSum(nums);
+
+        print1DArray(sumRows);
+
+        int sum = sumCol(nums, 1);
+        System.out.println();
+        System.out.println(sum);
+
+        int[] sumCols = arrColSum(nums);
+        print1DArray(sumCols);
+
+    }
+
+    private static int[] arrColSum(int[][] arr2D) {
+        int[] result = new int[arr2D[0].length];
+
+        for (int col = 0; col < result.length; col++) {
+            result[col] = sumCol(arr2D, col);
+        }
+
+        return result;
+    }
+
+    private static int sumCol(int[][] arr2D, int col) {
+        int sum = 0;
+
+        for (int[] row : arr2D) {
+            sum += row[col];
+        }
+
+        return sum;
+    }
+
+    private static int[] arrRowSum(int[][] arr2D) {
+        int[] result = new int[arr2D.length]; // arr2D.length is the number of rows
+
+        for (int row = 0; row < arr2D.length; row++) {
+            for (int col = 0; col < arr2D[row].length; col++) {
+                result[row] += arr2D[row][col];
+            }
+        }
+
+        return result;
+
     }
 
     private static void populate2DArray(int[][] arr2D) {
         for (int row = 0; row < arr2D.length; row++) {
             for (int col = 0; col < arr2D[row].length; col++) {
-                arr2D[row][col] = (int)(Math.random() * 10) + 1;
+                arr2D[row][col] = (int) (Math.random() * 10) + 1;
             }
             System.out.println();
         }
